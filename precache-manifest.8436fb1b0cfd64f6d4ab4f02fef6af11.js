3a9d779fe2bac6f8284324be7d65374f3406e312
Raemond <raemondcarta@gmail.com>
2023-02-20 21:12:42 -0500

    Updates

---
+++ precache-manifest.8436fb1b0cfd64f6d4ab4f02fef6af11.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6580566ab2a847889738",
+    "url": "/pizza/static/css/main.c14af294.chunk.css"
+  },
+  {
+    "revision": "6580566ab2a847889738",
+    "url": "/pizza/static/js/main.6580566a.chunk.js"
+  },
+  {
+    "revision": "846760e967b0356cd2b8",
+    "url": "/pizza/static/js/1.846760e9.chunk.js"
+  },
+  {
+    "revision": "813fbdcf5b8bdf54f3ce",
+    "url": "/pizza/static/css/2.23e2da0e.chunk.css"
+  },
+  {
+    "revision": "813fbdcf5b8bdf54f3ce",
+    "url": "/pizza/static/js/2.813fbdcf.chunk.js"
+  },
+  {
+    "revision": "f38e9f863abc579bd213",
+    "url": "/pizza/static/js/runtime~main.f38e9f86.js"
+  },
+  {
+    "revision": "503be8653631caf7097dd0fda73e63fc",
+    "url": "/pizza/static/media/pizza.503be865.jpg"
+  },
+  {
+    "revision": "8d1c7ef79a18735d859a8bdd92c0ab0f",
+    "url": "/pizza/static/media/smile.8d1c7ef7.jpg"
+  },
+  {
+    "revision": "856ed5fd4f9c9fa4aafc7186d6b769d3",
+    "url": "/pizza/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/pizza/precache-manifest.8436fb1b0cfd64f6d4ab4f02fef6af11.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/pizza/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
